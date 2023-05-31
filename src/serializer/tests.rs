bf3260fa5246b95cfdead4af8128af303622d5a7
mrtnvgr <martynovegorOF@yandex.ru>
2023-05-31 14:57:10 +0700

    feat(tests): add debug prints

--- src/serializer/tests.rs
+++ src/serializer/tests.rs
@@ -4,26 +4,28 @@ use std::{env::temp_dir, fs, iter::zip};
 #[test]
 fn serialize_mrg() {
     let bytes = fs::read("levels.mrg").unwrap();
-    let old = Mrg::from_bytes(bytes).unwrap();
+    let old = Mrg::from_bytes(bytes.clone()).unwrap();
 
     let new_bytes = old.to_bytes().unwrap();
     fs::write(temp_dir().join("test.mrg"), &new_bytes).unwrap();
 
-    let new = Mrg::from_bytes(new_bytes).unwrap();
+    let new = Mrg::from_bytes(new_bytes.clone()).unwrap();
 
     for (old_diff, new_diff) in zip(old.iter_diffs(), new.iter_diffs()) {
         for (old_track, new_track) in zip(&old_diff.tracks, &new_diff.tracks) {
+            // println!("Track: {}", new_track.name);
             assert_eq!(old_track.start.to_tuple(), new_track.start.to_tuple());
             assert_eq!(old_track.finish.to_tuple(), new_track.finish.to_tuple());
 
             assert_eq!(old_track.points.len(), new_track.points.len());
 
             for (old_point, new_point) in zip(&old_track.points, &new_track.points) {
+                // println!("{old_point} -> {new_point}");
                 assert_eq!(old_point.to_tuple(), new_point.to_tuple());
             }
         }
     }
 
     // FIXME: serializing should be lossless
-    // assert_eq!(mrg.to_bytes(), bytes, "Serializing failed");
+    // assert_eq!(bytes, new_bytes, "Serializing failed");
 }
